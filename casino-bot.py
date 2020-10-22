088a9949d001383642d327cc238c64e0902599cb
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2020-10-22 20:45:54 +0530

    First commit

---
+++ casino-bot.py
@@ -0,0 +1,38 @@
+import requests
+import random
+import time
+import os
+from config import *
+
+url = "https://discord.com/api/v8/channels/" + channel_id + "/messages"
+
+cookies = {
+    '_ga': 'GA1.2.735792174.1600309507',
+    'locale': 'en-US',
+}
+
+headers = {
+    'Host': 'discord.com',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US',
+    'Accept-Encoding': 'gzip, deflate',
+    'Content-Type': 'application/json',
+    'Authorization': os.environ.get('CASINO_BOT_TOKEN'),
+    'Origin': 'https://discord.com',
+    'Connection': 'close',
+}
+
+def main(message):
+    data = '{"content":"' + message + '","nonce":' + str(random.randint(1000, 99999999))+',"tts":false}'
+    response = requests.post(url, headers=headers, cookies=cookies, data=data)
+    return response.content
+
+while True:
+    try:
+    	main('*work')
+    	main('*dep all')
+    	time.sleep(30)
+    	continue
+    except:
+	    pass
