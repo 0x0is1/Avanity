4678d84aa335d6e1637eeffc7ec78b74fdccf2db
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2020-10-25 17:13:41 +0530

    [*] Completed config file

--- config.py
+++ config.py
@@ -1,2 +1,15 @@
-channel_id = "765221940667219988"
+# Copy channel id from discord
+channel_id = "750836633456214127"
+
+# next collect cooldown time
 cooldown_time = 30
+
+# some casino bots take time to recieve next command or slowmode
+wait_duration = 1
+
+# change commands to True to if you want to enable it
+# Note: crime and slut command can give you huge fine 
+# if you have a huge amount in casino
+crime = False
+slut = False
+work = True
